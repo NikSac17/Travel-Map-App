c7465f928e482b1811e7eeb6622efd9354dd5730
Nikhil Sachan <nsachan1983@gmail.com>
2021-09-27 10:51:19 +0530

    added mapbox

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import ReactMapGL, { Marker } from "react-map-gl";
+import {Room} from "@material-ui/icons"
 
 function App() {
+  const [viewport, setViewport] = useState({
+    width: "100vw",
+    height: "100vw",
+    latitude: 26.4499,
+    longitude: 80.3319,
+    zoom: 4,
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
+        onViewportChange={(nextViewport) => setViewport(nextViewport)}
+        mapStyle="mapbox://styles/nikhilsachan12/cku279kba3h1617p49n5ws6bk"
+      >
+        <Marker
+          latitude={48.85}
+          longitude={2.29}
+          offsetLeft={-20}
+          offsetTop={-10}
         >
-          Learn React
-        </a>
-      </header>
+          <Room style={{fontSize: viewport.zoom * 5}}/>
+        </Marker>
+      </ReactMapGL>
     </div>
   );
 }
