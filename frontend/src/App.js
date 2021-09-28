76ba1d58da49d29c6d0143789fb89ad2a15c4571
Nikhil Sachan <nsachan1983@gmail.com>
2021-09-28 09:15:44 +0530

    fetching pin data from backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
-import ReactMapGL, { Marker } from "react-map-gl";
-import {Room} from "@material-ui/icons"
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import { Room, Star } from "@material-ui/icons";
+import { format } from "timeago.js";
 
 function App() {
+  const currUser = "NikSac";
+  const [pins, setPins] = useState([]);
+  const [currLoc, setCurrLoc] = useState(null);
+
   const [viewport, setViewport] = useState({
     width: "100vw",
-    height: "100vw",
+    height: "100vh",
     latitude: 26.4499,
     longitude: 80.3319,
     zoom: 4,
   });
+
+  useEffect(() => {
+    const getPins = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/pins");
+        const json = await response.json();
+        setPins(json);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getPins();
+  }, []);
+
+  const handleClick = (id) => {
+    setCurrLoc(id);
+  };
 
   return (
     <div>
@@ -20,14 +42,49 @@ function App() {
         onViewportChange={(nextViewport) => setViewport(nextViewport)}
         mapStyle="mapbox://styles/nikhilsachan12/cku279kba3h1617p49n5ws6bk"
       >
-        <Marker
-          latitude={48.85}
-          longitude={2.29}
-          offsetLeft={-20}
-          offsetTop={-10}
-        >
-          <Room style={{fontSize: viewport.zoom * 5}}/>
-        </Marker>
+        {pins.map((pin) => {
+          return (
+            <>
+              <Marker
+                latitude={pin.lat}
+                longitude={pin.long}
+                offsetLeft={-20}
+                offsetTop={-10}
+              >
+                <Room
+                  onClick={() => handleClick(pin._id)}
+                  style={{ fontSize: viewport.zoom * 5, color: pin.username===currUser? "tomato" : "blue", cursor: "pointer"}}
+                />
+              </Marker>
+              {pin._id === currLoc && (
+                <Popup
+                  latitude={pin.lat}
+                  longitude={pin.long}
+                  closeButton={true}
+                  closeOnClick={false}
+                  anchor="left"
+                  onClose={()=>setCurrLoc(null)} 
+                >
+                  {" "}
+                  <div>
+                    <label>Place</label>
+                    <h4>{pin.title}</h4>
+                    <label>Review</label>
+                    <p>{pin.description}</p>
+                    <div>
+                      <Star />
+                      <Star />
+                      <Star />
+                    </div>
+                    <label>Info</label>
+                    <span>{pin.username}</span>
+                    <span>{format(pin.timestamp)}</span>
+                  </div>
+                </Popup>
+              )}
+            </>
+          );
+        })}
       </ReactMapGL>
     </div>
   );
